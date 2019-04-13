180f5462a93ea6259a1f38519ae65a76b20261a1
Xue <chengdexy@outlook.com>
2019-04-13 12:20:45 +0800

    去除签名文件

--- XMarker.Dll/MarkPrinter.cs
+++ XMarker.Dll/MarkPrinter.cs
@@ -3,8 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Drawing.Printing;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace XMarker.Dll
